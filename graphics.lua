059d358f6637da0077b5de60f6e93efe30d3c9fe
Lucian Parfeni <darcu.ro@gmail.com>
2023-11-30 13:08:13 +0200

    Graphics

--- graphics.lua
+++ graphics.lua
@@ -1,36 +1,32 @@
 local gfx <const> = playdate.graphics
 
-
----@param image _Image
----@param cx number?
----@param cy number?
+---@param params { image: _Image, cx: number?, cy: number? }
 ---@return _Sprite
-local function initSprite(image, cx, cy)
-  local sprite = gfx.sprite.new(image)
+local function initSprite(params)
+  local sprite = gfx.sprite.new(params.image)
 
-  sprite:setCenter(cx or 0, cy or 0)
+  sprite:setCenter(params.cx or 0, params.cy or 0)
 
   return sprite
 end
 
 
----@param sprite _Sprite
----@param x number
----@param y number
-local function addSpriteAt(sprite, x, y)
-  sprite:moveTo(x, y)
-  sprite:add()
+---@param params { sprite: _Sprite, x: number, y: number }
+---@return _Sprite
+local function addSpriteAt(params)
+  params.sprite:moveTo(params.x, params.y)
+  params.sprite:add()
 
-  return sprite
+  return params.sprite
 end
 
 
 ---@param params { image: _Image, x: number, y: number }
 ---@return _Sprite
 local function instanceSprite(params)
-  local sprite = initSprite(params.image)
+  local sprite = initSprite({ image = params.image })
 
-  return addSpriteAt(sprite, params.x, params.y)
+  return addSpriteAt({ sprite = sprite, x = params.x, y = params.y })
 end
 
 
@@ -54,8 +50,8 @@ end
 
 
 ---@class _Graphics
----@field initSprite fun(image: _Image, cx: number?, cy: number?): _Sprite
----@field addSpriteAt fun(sprite: _Sprite, x: number, y: number): _Sprite
+---@field initSprite fun(params: { image: _Image, cx: number?, cy: number? }): _Sprite
+---@field addSpriteAt fun(params: { sprite: _Sprite, x: number, y: number }): _Sprite
 ---@field instanceSprite fun(params: { image: _Image, x: number, y: number }): _Sprite
 ---@field setBg fun(bgCallback: function)
 ---@field drawCenteredText fun(text: string, y: number)
